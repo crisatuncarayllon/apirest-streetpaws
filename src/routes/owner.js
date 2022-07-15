1e794e6def3a06bc6d817f072ddc1a7a1816deb2
crisatuncarayllon <58598125+crisatuncarayllon@users.noreply.github.com>
2022-07-15 18:16:09 -0500

    Uso de async en ruta owner

--- src/routes/owner.js
+++ src/routes/owner.js
@@ -4,50 +4,60 @@ const ownerSchema = require('../models/owner');
 const router = express.Router();
 
 //Create an owner
-router.post('/owners',(req, res)=>{
-    const owner = ownerSchema(req.body);
-    owner
-    .save()
-    .then((data)=>res.json(data))
-    .catch((error)=>res.json({message:error}));
+router.post('/owners',async(req, res)=>{
+    try {
+        const owner = ownerSchema(req.body);
+        const newOwner = await owner.save();
+        res.status(200).json(newOwner);
+    } catch (error) {
+        res.json({message:error});   
+    }
 });
 
 //Get all owners
-router.get('/owners',(req, res)=>{
-    ownerSchema
-    .find()
-    .then((data)=>res.json(data))
-    .catch((error)=>res.json({message:error}));
+router.get('/owners',async(req, res)=>{
+    try {
+        const owners = await ownerSchema.find(); 
+        res.status(200).json(owners);
+    } catch (error) {
+        res.json({message:error});
+    }
 });
 
 //Get an owner by id
-router.get('/owners/:id',(req, res)=>{
-    const { id } = req.params;
-    ownerSchema
-    .findById(id)
-    .then((data)=>res.json(data))
-    .catch((error)=>res.json({message:error}));
+router.get('/owners/:id',async(req, res)=>{
+    try {
+        const { id } = req.params;
+        const owner = await ownerSchema.findById(id);
+        res.status(200).json(owner);
+    } catch (error) {
+        res.json({message:error});
+    }
 });
 
 //update an owner by id
 //No se modifica el arreglo de "dogs" el cual almacena un arreglo de los perros que pertenecen a cada dueño
-router.put('/owners/:id',(req, res)=>{
-    const { id } = req.params;
-    const { name, dni, address, email, status } = req.body;
-    ownerSchema
-    .updateOne({_id: id}, { $set: { name, dni, address, email, status } })
-    .then((data)=>res.json(data))
-    .catch((error)=>res.json({message:error}));
+router.put('/owners/:id',async(req, res)=>{
+    try {
+        const { id } = req.params;
+        const { name, dni, address, email, status } = req.body;
+        const ownerUpdated = await ownerSchema.updateOne({_id: id}, { $set: { name, dni, address, email, status } });
+        res.status(200).json(ownerUpdated);
+    } catch (error) {
+        (error)=>res.json({message:error});
+    }
 });
 
 
 //Delete an owner by id
-router.delete('/owners/:id',(req, res)=>{
-    const { id } = req.params;
-    ownerSchema
-    .remove({_id: id})
-    .then((data)=>res.json(data))
-    .catch((error)=>res.json({message:error}));
+router.delete('/owners/:id',async(req, res)=>{
+    try {
+        const { id } = req.params;
+        const ownerDeleted = await ownerSchema.remove({_id: id});
+        res.status(200).json(ownerDeleted);
+    } catch (error) {
+        res.json({message:error});
+    }
 });
 
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -49,8 +49,8 @@ router.get('/users',(req, res)=>{
 
 //Get a user by id
 router.get('/users/:id', async(req, res)=>{
-    const { id } = req.params;
     try {
+        const { id } = req.params;
         const user = await userSchema.findById(id);
         res.status(200).json(user);
     } catch (error) {
